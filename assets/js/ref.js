5aaa52f076df5b4c9661120523a66dda0de9ea04
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2023-01-12 13:07:20 +0530

    pushing

---
+++ assets/js/ref.js
@@ -0,0 +1,44 @@
+let courseMonitoryGithub = () => {
+  window.open(
+    'https://github.com/pranshubisht9/nifty-competition-2668',
+    '_blank'
+  );
+
+  }
+let KettoGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/ketto.org.git',
+        '_blank'
+      );
+}
+
+let InventoryGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Inventory_Admin_User',
+        '_blank'
+      );
+}
+
+let CodeCovGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/codeCov.io',
+        '_blank'
+      );
+}
+
+let StarWarGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Star_Wars',
+        '_blank'
+      );
+}
+let myResume = () =>{
+  window.open(
+    'https://drive.google.com/file/d/1Q3WVb0V6vF0e5ftaBXwE5QEd-TQFd5X7/view?usp=sharing',
+    '_blank'
+  );
+}
+
+GitHubCalendar(".calendar", "pranshubisht9");
+  // or enable responsive functionality
+  GitHubCalendar(".calendar", "pranshubisht9", { responsive: true });
